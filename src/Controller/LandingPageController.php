940dc033d112aac1bfa0c73629feab610c68212f
Nihel Ben Mahfoudh <nihel.belhadj@gmail.com>
2025-02-11 16:56:58 +0100

    Ajout de la vérification des rôles pour l'accès à la page de destination, en restreignant l'accès aux utilisateurs avec les rôles ROLE_PRO et ROLE_AGENT.

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -5,19 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class LandingPageController extends AbstractController
 {
     #[Route('/landing/add', name: 'lp_add')]
     public function add(): Response
     {   
-        $user = $this->getUser();
-        if(
-            !in_array('ROLE_AGENT', $user->getRoles()) ||
-            !in_array('ROLE_PRO', $user->getRoles())
-            ){
+        $user = $this->getUser()->getRoles()[0];
+
+        if($user !== "ROLE_PRO" && $user !== "ROLE_AGENT") {
                 return $this->redirectToRoute('app_detail');
         }
+        
         return $this->render('landing_page/index.html.twig', [
             'controller_name' => 'LandingPageController',
         ]);
