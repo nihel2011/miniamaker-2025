0610fe88b46b04b15ba5b1cf2df60f3d8b60cd40
Nihel Ben Mahfoudh <nihel.belhadj@gmail.com>
2025-02-11 15:38:31 +0100

    Ajout de nouveaux fichiers de configuration pour Liip Imagine et Twig Component, ajout d'une icône Symfony, et mise à jour des styles CSS pour les composants.

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,9 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     Symfony\UX\Cropperjs\CropperjsBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
